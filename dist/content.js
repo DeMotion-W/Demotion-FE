81f8643b839885cc394786f0e339bdc249ac1df4
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2025-03-29 14:46:29 +0900

    feat: Add image capture function in chrome extension page #2 - 크롬 익스텐션 페이지에서 이미지 캡처 기능 구현 - manifest.json 관리 방식 재설정

---
+++ dist/content.js
@@ -0,0 +1,29 @@
+let isCapturing = false;
+
+const onClick = (e) => {
+  if (!isCapturing) return;
+
+  chrome.runtime.sendMessage({
+    type: "capture-click",
+    x: e.clientX,
+    y: e.clientY,
+  });
+};
+
+chrome.runtime.onMessage.addListener(
+  (msg, sender, sendResponse) => {
+    if (msg.type === "start-capture") {
+      if (!isCapturing) {
+        isCapturing = true;
+        document.body.style.cursor = "crosshair";
+        document.addEventListener("click", onClick);
+      }
+    }
+
+    if (msg.type === "stop-capture") {
+      isCapturing = false;
+      document.body.style.cursor = "default";
+      document.removeEventListener("click", onClick);
+    }
+  }
+);
--- extension/background.js
+++ extension/background.js
@@ -19,6 +19,27 @@ chrome.runtime.onInstalled.addListener(() => {
     () => {
       if (chrome.runtime.lastError) {
         console.error(
+          "알림 오류:",
+          chrome.runtime.lastError.message
+        );
+      }
+    }
+  );
+});
+
+chrome.action.onClicked.addListener(() => {
+  chrome.notifications.create(
+    "demotion-welcome",
+    {
+      type: "basic",
+      iconUrl: chrome.runtime.getURL("icons/icons-48.png"),
+      title: "Demotion 시작!",
+      message:
+        "우측 확장 아이콘을 눌러 사이드 패널을 확인해보세요!",
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(
           "🔴 알림 오류:",
           chrome.runtime.lastError.message
         );
@@ -26,3 +47,21 @@ chrome.runtime.onInstalled.addListener(() => {
     }
   );
 });
+
+chrome.runtime.onMessage.addListener(
+  async (msg, sender) => {
+    if (msg.type === "capture-click") {
+      const screenshot =
+        await chrome.tabs.captureVisibleTab();
+
+      chrome.runtime.sendMessage({
+        type: "captured-image",
+        data: {
+          image: screenshot,
+          x: msg.x,
+          y: msg.y,
+        },
+      });
+    }
+  }
+);
